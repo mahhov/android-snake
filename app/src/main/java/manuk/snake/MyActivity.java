f9fc0c889ff5bbe0fe257e608129737f9c6e9a6c
Manuk Hovanesian <manuk.hovanesian@farmersinsurance.com>
2017-06-27 08:28:10 -0700

    fullscreen

--- app/src/main/java/manuk/snake/MyActivity.java
+++ app/src/main/java/manuk/snake/MyActivity.java
@@ -2,6 +2,8 @@ package manuk.snake;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MyActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
